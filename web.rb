5e3552907fa985cba190266022d84a80ec8a5aca
chidi.uba <talk2chidicfc@yahoo.com>
2014-12-19 22:46:58 +0000

    authorised access working

--- web.rb
+++ web.rb
@@ -7,7 +7,7 @@ require_all "app"
 
 enable :sessions
 
-get "/" do
+get '/' do
   erb :index
 end
 
@@ -16,11 +16,14 @@ post '/login' do
   view = UserView.new params[:name], params[:password]
   controller = UserController.new view
   unless controller.isUser?
-    
+    session[:user] = view
+    redirect "/secret-santa"
   else
     flash[:error] = "Unauthorised access"
     redirect "/"
   end
+end
 
-
+get '/secret-santa' do
+  erb :secret_santa
 end
